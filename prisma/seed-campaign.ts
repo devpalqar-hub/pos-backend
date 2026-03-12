bddd6725ea513c38adc9c72551989ae9928246f2
DevanandGit <nandhudevanand4419@gmail.com>
2026-03-12 11:50:33 +0530

    seeder added 5

--- prisma/seed-campaign.ts
+++ prisma/seed-campaign.ts
@@ -3,8 +3,8 @@ import { OrderChannel } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-// const restaurantId = "c5f50dda-222a-445b-a41d-4f1a31914cf9"
-const restaurantId = "110201cd-698d-4ad9-9c63-4ea706d95f8f"
+const restaurantId = "c5f50dda-222a-445b-a41d-4f1a31914cf9"
+// const restaurantId = "110201cd-698d-4ad9-9c63-4ea706d95f8f"
 
 const customers = [
     {
